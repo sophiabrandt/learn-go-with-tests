af888295b936d016f022fa8c2760630cb84a709e
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-10-22 16:03:48 +0200

    feat: print "Hello, world" if no name is provided

--- 01-hello-world/hello.go
+++ 01-hello-world/hello.go
@@ -5,6 +5,9 @@ import "fmt"
 const helloPrefixEN = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return helloPrefixEN + name
 }
 
--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("World")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
