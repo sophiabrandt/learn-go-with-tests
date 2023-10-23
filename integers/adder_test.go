302cea85de17e485e1f64f7a723ea28a61c590ec
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-10-23 21:13:08 +0200

    feat: add integer test

---
+++ integers/adder_test.go
@@ -0,0 +1,23 @@
+package integers_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sophiabrandt/learn-go-with-tests/integers"
+)
+
+func TestAdder(t *testing.T) {
+	sum := integers.Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := integers.Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
